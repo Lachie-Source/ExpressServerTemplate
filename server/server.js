f90368e6938b2fc4da81dc538098f14f93356a20
Lachie-Source <62362511+Lachie-Source@users.noreply.github.com>
2020-06-11 08:21:21 +1000

    epico

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 
 // Setting up the routes
-const routes = { POST: require("./routes/post"), GET: require("./routes/get") };
+const routes = { POST: require("./routes/post"), GET: require("./routes/get") }; // These routes can be any name, and they dont need to be seperated into eachh method
 
 app.use("/post", routes.POST);
 app.use("/get", routes.GET);
@@ -15,9 +15,15 @@ app.use("/get", routes.GET);
 // Declaring The Port
 const PORT = process.env.PORT || 8000; // The alternitive port can be any four-digit number
 
-app.get("/", express.static("server/views")); // Here we are loading some html
+app.get("/", express.static("server/views/example")); // Here we are loading some html
 
-// Handling 404 Errors
+app.put("/auth/submit", (req, res) => {
+  console.log(req);
+  res.send("pp");
+});
+app.use("/auth", express.static("server/views/auth"));
+
+// Handling 404 errors (Always keep this before we listen to the port)
 app.use((req, res) => {
   res.status(404).send("404 Page Not Found");
 });
